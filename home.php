28951e30b387655cdd2a8301a81addfbc44226c6
bima laksmana <bima.laksmana@gmail.com>
2013-10-29 14:06:35 +0700

    Header + Kategori

--- home.php
+++ home.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html> 
 <html>
 <head>
+	<title>Ruko Serba ada - Home</title>
 	<link rel="stylesheet" type="text/css" href="layout.css">
 	
 </head>
 
 <body>
+	
+	<audio autoplay loop>
+		<source src="kungfu1.mp3" type="audio/mpeg">
+	</audio>
 	<!-- Header page -->
 		<header>
 		<div id="icon" style="float:left; padding-left:15px; padding-right:15px;
@@ -19,7 +24,7 @@
 		font-size:40px; color:white; float:left;
 		padding-top:60px; padding-left:40px; padding-right:40px;
 		margin-left:2px; background-color:#FF0000">
-			Showing result(s) for : "<?php $category = 'Mie Instant'; echo $category; ?>"
+			Ruko Serba Ada
 		</div>
 
 		<div id="head-right" style="width:18%; height:155px; float:left;
@@ -29,7 +34,7 @@
 				<a href="Cart.php">Go to Cart</a>
 				<div id="login" style="float: right; text-align:right">
 					<a href="Login.php">Login</a><br>
-					<a href="Register.php">Register</a>
+					<a href="registrasi_user.php">Register</a>
 				</div>
 				<div id="logout" style="float: right; text-align:right; display:none">
 					<a onclick="logout()" href="Halaman Barang.php">Logout</a><br>
@@ -47,9 +52,20 @@
 				</form>
 			</div>
 		</div>
+		<div id="navcontainer">
+			<ul id="navlist">
+				
+				<li id="active"><a href="Halaman_Barang.php" id="current">Beras</a></li>
+				<li><a href="Halaman_Barang.php">Mie</a></li>
+				<li><a href="Halaman_Barang.php">Daging</a></li>
+				<li><a href="Halaman_Barang.php">Kecap</a></li>
+				<li><a href="Halaman_Barang.php">Motor</a></li>
+			</ul>
+		</div>
+				
 		<!-- Sampai sini headernya udah beres -->
 		</header>
-
+	<div id ="container"> 
 	<div class = "top5title">
 		<label> Top Three Beras<label></br>
 	</div>
@@ -60,25 +76,27 @@
 			//{
 				echo'<div class = "firstpreview" >
 						<div class = "nextimage">
-							<a href="index.php"><img src="Dummy/BerasRambutan.jpg" class="resizeimage"></a>
+							<a href="Halaman_Barang.php"><img src="Dummy/BerasRambutan.jpg" class="resizeimage"></a>
 						<p class = "centertext"> Beras Rambutan</br></p>
 						</div>
 					</div>
 					<div class = "firstpreview" >
 						<div class = "nextimage">
-							<a href="index.php"><img src="Dummy/BerasC4Super.jpg" class="resizeimage"></a>
+							<a href="Halaman_Barang.php"><img src="Dummy/BerasC4Super.jpg" class="resizeimage"></a>
 						<p class = "centertext"> Beras C4 Super</br></p>
 						</div>
 					</div>
 					<div class = "firstpreview" >
 						<div class = "nextimage">
-							<a href="index.php"><img src="Dummy/BerasCapKapak.jpg" class="resizeimage"></a>
+							<a href="Halaman_Barang.php"><img src="Dummy/BerasCapKapak.jpg" class="resizeimage"></a>
 						<p class = "centertext"> Beras Cap Kapak</br></p>
 						</div>
 					</div>';
 		?>			
 	</div>
+	</div>
 	
+	<div id="container">
 	<div class = "top5title">
 		<label> Top Three Mie<label></br>
 	</div>
@@ -89,25 +107,26 @@
 			//{
 				echo'<div class = "firstpreview" >
 						<div class = "nextimage">
-							<a href="index.php"><img src="Dummy/MieMentah.jpg" class="resizeimage"></a>
+							<a href="Halaman_Barang.php"><img src="Dummy/MieMentah.jpg" class="resizeimage"></a>
 						<p class = "centertext"> Mie Mentah</br></p>
 						</div>
 					</div>
 					<div class = "firstpreview" >
 						<div class = "nextimage">
-							<a href="index.php"><img src="Dummy/MieGoreng.jpg" class="resizeimage"></a>
+							<a href="Halaman_Barang.php"><img src="Dummy/MieGoreng.jpg" class="resizeimage"></a>
 						<p class = "centertext"> Mie Goreng</br></p>
 						</div>
 					</div>
 					<div class = "firstpreview" >
 						<div class = "nextimage">
-							<a href="index.php"><img src="Dummy/MieRebus.jpg" class="resizeimage"></a>
+							<a href="Halaman_Barang.php"><img src="Dummy/MieRebus.jpg" class="resizeimage"></a>
 						<p class = "centertext"> Mie Rebus</br></p>
 						</div>
 					</div>';
 		?>			
 	</div>
-	
+	</div>
+	<div id="container">
 	<div class = "top5title">
 		<label> Top Three Beras<label></br>
 	</div>
@@ -118,24 +137,26 @@
 			//{
 				echo'<div class = "firstpreview" >
 						<div class = "nextimage">
-							<a href="index.php"><img src="Dummy/BerasRambutan.jpg" class="resizeimage"></a>
+							<a href="Halaman_Barang.php"><img src="Dummy/BerasRambutan.jpg" class="resizeimage"></a>
 						<p class = "centertext"> Beras Rambutan</br></p>
 						</div>
 					</div>
 					<div class = "firstpreview" >
 						<div class = "nextimage">
-							<a href="index.php"><img src="Dummy/BerasC4Super.jpg" class="resizeimage"></a>
+							<a href="Halaman_Barang.php"><img src="Dummy/BerasC4Super.jpg" class="resizeimage"></a>
 						<p class = "centertext"> Beras C4 Super</br></p>
 						</div>
 					</div>
 					<div class = "firstpreview" >
 						<div class = "nextimage">
-							<a href="index.php"><img src="Dummy/BerasCapKapak.jpg" class="resizeimage"></a>
+							<a href="Halaman_Barang.php"><img src="Dummy/BerasCapKapak.jpg" class="resizeimage"></a>
 						<p class = "centertext"> Beras Cap Kapak</br></p>
 						</div>
 					</div>';
 		?>			
 	</div>
+	</div>
+	<div id="container">
 	<div class = "top5title">
 		<label> Top Three Mie<label></br>
 	</div>
@@ -146,25 +167,26 @@
 			//{
 				echo'<div class = "firstpreview" >
 						<div class = "nextimage">
-							<a href="index.php"><img src="Dummy/MieMentah.jpg" class="resizeimage"></a>
+							<a href="Halaman_Barang.php"><img src="Dummy/MieMentah.jpg" class="resizeimage"></a>
 						<p class = "centertext"> Mie Mentah</br></p>
 						</div>
 					</div>
 					<div class = "firstpreview" >
 						<div class = "nextimage">
-							<a href="index.php"><img src="Dummy/MieGoreng.jpg" class="resizeimage"></a>
+							<a href="Halaman_Barang.php"><img src="Dummy/MieGoreng.jpg" class="resizeimage"></a>
 						<p class = "centertext"> Mie Goreng</br></p>
 						</div>
 					</div>
 					<div class = "firstpreview" >
 						<div class = "nextimage">
-							<a href="index.php"><img src="Dummy/MieRebus.jpg" class="resizeimage"></a>
+							<a href="Halaman_Barang.php"><img src="Dummy/MieRebus.jpg" class="resizeimage"></a>
 						<p class = "centertext"> Mie Rebus</br></p>
 						</div>
 					</div>';
 		?>			
 	</div>
-	
+	</div>
+	<div id="container">
 	<div class = "top5title">
 		<label> Top Three Beras<label></br>
 	</div>
@@ -175,25 +197,25 @@
 			//{
 				echo'<div class = "firstpreview" >
 						<div class = "nextimage">
-							<a href="index.php"><img src="Dummy/BerasRambutan.jpg" class="resizeimage"></a>
+							<a href="Halaman_Barang.php"><img src="Dummy/BerasRambutan.jpg" class="resizeimage"></a>
 						<p class = "centertext"> Beras Rambutan</br></p>
 						</div>
 					</div>
 					<div class = "firstpreview" >
 						<div class = "nextimage">
-							<a href="index.php"><img src="Dummy/BerasC4Super.jpg" class="resizeimage"></a>
+							<a href="Halaman_Barang.php"><img src="Dummy/BerasC4Super.jpg" class="resizeimage"></a>
 						<p class = "centertext"> Beras C4 Super</br></p>
 						</div>
 					</div>
 					<div class = "firstpreview" >
 						<div class = "nextimage">
-							<a href="index.php"><img src="Dummy/BerasCapKapak.jpg" class="resizeimage"></a>
+							<a href="Halaman_Barang.php"><img src="Dummy/BerasCapKapak.jpg" class="resizeimage"></a>
 						<p class = "centertext"> Beras Cap Kapak</br></p>
 						</div>
 					</div>';
 		?>			
 	</div>
-		
+	</div>
 
 
 	<div id="footer">
@@ -206,7 +228,7 @@
 	</div>
 	<p><b>Copyright © Krisna Fathurahman, Mochammad Dikra Prasetya, Bima Laksmana</b></p>
 	</div>
-
+	
 </body>
 
 </html>
